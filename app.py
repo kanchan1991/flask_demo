8073134bbce93993ad0a2729538cd789e7f4f7da
Kanchan Srivastava <kanchan.srivastava@cdk.com>
2017-01-17 11:56:05 +0530

    link between pages

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
-
 guesses = ['Python', 'Java', 'C++']
+questions = ['Is it compiled?', 'Does it run a VM?']
 
 
 @app.route('/')
@@ -16,6 +16,11 @@ def index():
 @app.route('/guess/<int:id>')
 def guess(id):
     return render_template('guess.html', guess=guesses[id])
+
+
+@app.route('/question/<int:id>')
+def question(id):
+    return render_template('question.html', question=questions[id])
 
 
 if __name__ == '__main__':
