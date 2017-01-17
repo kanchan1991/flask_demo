d2057854ed4ac8f0ee76a3ad2ebebc054c495e02
Kanchan Srivastava <kanchan.srivastava@cdk.com>
2017-01-17 11:11:05 +0530

    dynamic behaviour supporting views and urls

--- app.py
+++ app.py
@@ -5,10 +5,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+guesses = ['Python', 'Java', 'C++']
+
 
 @app.route('/')
 def index():
-    return 'Hello Flask!'
+    return '<h1>Guess the language</h1>'
+
+
+@app.route('/guess/<int:id>')
+def guess(id):
+    return ('<h1>Guess the language</h1>'
+            '<p>My guess: {0}</p>'.format(guesses[id]))
 
 
 if __name__ == '__main__':
